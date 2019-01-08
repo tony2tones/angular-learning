6983dd2f444e2bc2ad4c56c01245c1ddb514744a
Anthony <anthony.marque@gmail.com>
2019-01-08 15:27:21 +0200

    added template and courses obj

--- starter/src/app/courses.component.ts
+++ starter/src/app/courses.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>{{ title }}</h2>
+         <ul>
+            <li *ngFor="let course of courses"> 
+            {{ course }}
+            </li>
+        </ul>`
 })
 
 export class CoursesComponent {
-
+    title = "List of courses";
+    courses = ['course 1','course 2','course 3']
 }
