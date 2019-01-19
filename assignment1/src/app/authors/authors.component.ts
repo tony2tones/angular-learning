09116e7fe8e32c74ac3e4c8d7695e9c971f982a9
Anthony <anthonymarques@outlook.com>
2019-01-19 16:47:08 +0200

    added authors array and used interpilation for rendering

---
+++ assignment1/src/app/authors/authors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent {
+authors = ['Author 1','Author 2','Author 3'];
+
+}
