43947f776c742d3cd92c60ca804c0e62e97993f9
Anthony <anthonymarques@outlook.com>
2019-01-22 21:24:59 +0200

    trying to add bootstrap

--- application-form/src/app/contact-form/contact-form.component.ts
+++ application-form/src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-contact-form',
+  selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ContactFormComponent {
 
 }
