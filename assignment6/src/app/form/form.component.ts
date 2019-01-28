f95b995143203c9e24cc21e83ffbe18e7d042dab
Anthony <anthony.marque@gmail.com>
2019-01-28 07:49:23 +0200

    added bootstrap stylings and added bootstrap packaged via npm

--- assignment6/src/app/form/form.component.ts
+++ assignment6/src/app/form/form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-form',
+  selector: 'form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
