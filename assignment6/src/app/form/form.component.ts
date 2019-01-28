fa19ca175b43dc69cab5b063bc00881115991c63
Anthony <anthony.marque@gmail.com>
2019-01-28 09:04:42 +0200

    adding validation to field

--- assignment6/src/app/form/form.component.ts
+++ assignment6/src/app/form/form.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
 
-  constructor() { }
+  log(x){
+    console.log(x);
 
-  ngOnInit() {
   }
 
 }
