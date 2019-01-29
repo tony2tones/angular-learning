8150754fef31dc720343c7e54b9127bca6d74941
Anthony <anthonymarques@outlook.com>
2019-01-29 21:32:46 +0200

    added submit button

--- assignment6/src/app/form/form.component.ts
+++ assignment6/src/app/form/form.component.ts
@@ -1,16 +1,18 @@
-import { Component} from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.css"]
 })
 export class FormComponent {
-  categories = ["Anuglar","React","Vuejs"]
-  
-  log(x){
+  categories = [
+    { id: 1, name: "Anuglar" },
+    { id: 1, name: "React" },
+    { id: 1, name: "Vue" }
+  ];
+
+  submit(x) {
     console.log(x);
-
   }
-
 }
