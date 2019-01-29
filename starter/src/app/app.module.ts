2bfc6c01e58fc4564b97f9b9034b3d22c36a815e
Anthony <anthonymarques@outlook.com>
2019-01-29 23:36:13 +0200

    added stylings and added a declaration

--- starter/src/app/app.module.ts
+++ starter/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { ZippyComponent } from './zippy/zippy.component';
     LikeComponent,
     InputFormatDirective,
     ZippyComponent,
+    SignupFormComponent
     
   ],
   imports: [
