9a05faea6ff6b0000820b25b237ddd7c6fc4fddb
Anthony <anthonymarques@outlook.com>
2019-02-09 08:18:29 +0200

    added required validators and getters for components

--- assignment7/src/app/change-password/change-password.component.ts
+++ assignment7/src/app/change-password/change-password.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl ,Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'change-password',
@@ -8,9 +8,19 @@ import { FormGroup, FormControl ,Validators } from '@angular/forms';
 })
 export class ChangePasswordComponent {
   from = new FormGroup({
-    oldPassword: new FormControl(),
-    newPassword: new FormControl(),
-    confirmPassword: new FormControl()
-  })
+    oldPassword: new FormControl('', Validators.required),
+    newPassword: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required)
+  });
 
+  get oldPassword() {
+    return this.from.get('oldPassword');
+  }
+
+  get newPassword() {
+    return this.from.get('newPassword');
+  }
+  get confirmPassword() {
+    return this.from.get('confirmPassword');
+  }
 }
