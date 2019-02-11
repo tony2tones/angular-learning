1fac2d96646fb1c92907bc6c1a64a5ba15940f18
Anthony <anthony.marque@gmail.com>
2019-02-11 09:33:14 +0200

    Form control and added checks for form is valid and password match.

--- assignment7/src/app/change-password/changePassword.validators.ts
+++ assignment7/src/app/change-password/changePassword.validators.ts
@@ -4,25 +4,20 @@ export class changePasswordValidators {
   static oldPasswordCheck(
     oldPassword: AbstractControl
   ): Promise<ValidationErrors | null> {
-    return new Promise((resolve, rejected) => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         if (oldPassword.value != "1234") resolve({ oldPasswordCheck: true });
         else resolve(null);
       }, 2000);
     });
   }
-  static confirmPasswordCheck(
-    control: AbstractControl
-  ): Promise<ValidationErrors | null> {
-    return new Promise(resolve => {
-      setTimeout(() => {
+  static confirmPasswordCheck(control: AbstractControl) {
         let newPassword = control.get("newPassword");
         let confirmPassword = control.get("confirmPassword");
-        if (newPassword != confirmPassword)
-          resolve({ confirmPasswordCheck: true });
-        else
-        resolve(null);
-      }, 2000);
-    });
-  }
-}
+        if (newPassword.value !== confirmPassword.value)
+          return ({ confirmPasswordCheck: true });
+
+        return null;
+    };
+ }
+
