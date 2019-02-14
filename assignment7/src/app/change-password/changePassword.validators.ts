5d7b19a86c742a00cc01218764b56390d1c0a595
Anthony <anthonymarques@outlook.com>
2019-02-14 19:20:52 +0200

    Merge remote-tracking branch 'origin/master'

--- assignment7/src/app/change-password/changePassword.validators.ts
+++ assignment7/src/app/change-password/changePassword.validators.ts
@@ -4,20 +4,11 @@ export class changePasswordValidators {
   static oldPasswordCheck(
     oldPassword: AbstractControl
   ): Promise<ValidationErrors | null> {
-    return new Promise((resolve) => {
+    return new Promise((resolve, rejected) => {
       setTimeout(() => {
         if (oldPassword.value != "1234") resolve({ oldPasswordCheck: true });
         else resolve(null);
       }, 2000);
     });
   }
-  static confirmPasswordCheck(control: AbstractControl) {
-        let newPassword = control.get("newPassword");
-        let confirmPassword = control.get("confirmPassword");
-        if (newPassword.value !== confirmPassword.value)
-          return ({ confirmPasswordCheck: true });
-
-        return null;
-    };
- }
-
+}
