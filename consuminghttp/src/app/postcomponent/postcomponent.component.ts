01aabb275ac1c7a0a7aa2d0095ca7f98a2fc0870
Anthony Marques <anthony@armorica.gk>
2019-02-14 11:31:39 +0200

    added create post via http

--- consuminghttp/src/app/postcomponent/postcomponent.component.ts
+++ consuminghttp/src/app/postcomponent/postcomponent.component.ts
@@ -1,23 +1,31 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'postcomponent',
   templateUrl: './postcomponent.component.html',
   styleUrls: ['./postcomponent.component.css']
 })
-export class PostcomponentComponent implements OnInit {
+export class PostcomponentComponent {
 posts;
+private url = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: HttpClient) {
-    http.get('http://jsonplaceholder.typicode.com/posts')
+  constructor(private http: HttpClient) {
+    http.get(this.url)
     .subscribe(response => {
       this.posts = response
+      console.log(this.posts);
+    });
+  }   
+
+  createPost(input: HTMLInputElement) {
+    let post = { title:input.value };
+
+    this.http.post(this.url, JSON.stringify(post))
+    .subscribe(response => {
       console.log(response);
     });
-   }
-
-  ngOnInit() {
   }
+
 
 }
