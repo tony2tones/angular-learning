d7f6ad34befc13ea5ed5f273a10e9972e8afe181
Anthony Marques <anthony@armorica.gk>
2019-02-15 10:54:22 +0200

    created a service folder

--- consuminghttp/src/app/app.module.ts
+++ consuminghttp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { PostService } from './post.service';
+import { PostService } from './services/post.service';
 import { AppComponent } from './app.component';
 import { PostcomponentComponent } from './postcomponent/postcomponent.component';
 
