6cc2c6969330dbdcf89e1db84f1b656e05a3e585
Anthony Marques <anthony@armorica.gk>
2019-03-18 08:22:57 +0200

    adding changes

--- consuminghttp/src/app/app.module.ts
+++ consuminghttp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { PostService } from './services/post.service';
 import { AppComponent } from './app.component';
