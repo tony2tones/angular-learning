123d0f83448fbed0a77fba1cef4674cf77e06417
Anthony Marques <anthony@armorica.gk>
2019-03-18 15:19:21 +0200

    moved service  to a services folder

--- assignment1/src/app/authors/authors.component.ts
+++ assignment1/src/app/authors/authors.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AuthorsService } from '../authors.service';
+import { AuthorsService } from '../services/authors.service';
 
 @Component({
   selector: 'authors',
